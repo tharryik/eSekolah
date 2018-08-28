6c6c7a8fcf7e85072ea6b271714dfed59679c901
tharry.ik <tharry.ik@gmail.com>
2018-08-29 06:42:54 +0700

    First project commit. Based on Master Detail

---
+++ SCHL/SCHL/SCHL/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+using SCHL.Views;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace SCHL
+{
+	public partial class App : Application
+	{
+        static TodoItemDatabase database;
+
+        public App ()
+		{
+			InitializeComponent();
+
+
+			MainPage = new MainPage();
+		}        
+
+        public static TodoItemDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new TodoItemDatabase(
+                      Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TodoSQLite.db3"));
+                }
+                return database;
+            }
+        }
+
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
